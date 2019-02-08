c55b27b891b7adf62c02e10110fcf47c9bc1b096
unknown <ermanjunaidi1@gmail.com>
2019-02-08 23:55:48 +0700

    9- Enumerating Properties

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function Circle(radius) {
 }
 
 const circle = new Circle(10)
-circle.location = { x: 1 }
 
-const propertyName = 'center location'
-circle[propertyName] = { x: 1 }
-delete circle['location']
+for (let key in circle) {
+    if (typeof circle[key] !== 'function')
+        console.log(key, circle[key])
+}
+
+const keys = Object.keys(circle)
+console.log(keys)
+
+if ('radius' in circle)
+    console.log('circle has a radius.')
