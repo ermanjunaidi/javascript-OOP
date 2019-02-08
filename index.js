7c6fd08f3f6c6e3128fd157e86fdfb6b0509bdfb
unknown <ermanjunaidi1@gmail.com>
2019-02-08 20:49:20 +0700

    5- Constructor Property

--- index.js
+++ index.js
@@ -9,4 +9,13 @@ function createCircle(radius) {
 }
 
 const circle = createCircle(1)
-circle.draw();
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw')
+    }
+}
+
+const another = new Circle(1)
